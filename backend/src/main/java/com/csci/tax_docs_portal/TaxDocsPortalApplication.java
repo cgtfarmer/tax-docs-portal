f49e05beb6dfb3d994b84b0aa35c11576682541d
Christian Farmer <christian@taxstrategistsofamerica.com>
2026-01-28 14:14:49 -0600

    Scaffold backend

---
+++ backend/src/main/java/com/csci/tax_docs_portal/TaxDocsPortalApplication.java
@@ -0,0 +1,13 @@
+package com.csci.tax_docs_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxDocsPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxDocsPortalApplication.class, args);
+	}
+
+}
