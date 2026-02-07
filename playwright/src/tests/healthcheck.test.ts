27f37f4add68ff5e2b1e8cf5eee223af053095d6
Christian Farmer <christian@taxstrategistsofamerica.com>
2026-02-07 11:31:04 -0600

    Restructure Playwright file organization

--- playwright/src/tests/healthcheck.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('healthcheck', async ({ request }) => {
-  const response = await request.get('/actuator/info');
-
-  expect(response.ok()).toBeTruthy();
-});
