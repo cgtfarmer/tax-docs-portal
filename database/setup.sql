84f528cc88e931f1a11a079ef345873c64083272
mowery39 <kmallory03@gmail.com>
2026-02-23 17:41:30 -0600

    TDP-42/TDP-43: create accountants and clients tables

--- database/setup.sql
+++ database/setup.sql
@@ -1,3 +1,4 @@
+-- User Table
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -8,6 +9,31 @@ CREATE TABLE users (
   weight FLOAT,
   smoker BOOLEAN
 );
+
+-- Accountants Table 
+DROP TABLE IF EXISTS accountants;
+
+CREATE TABLE accountants (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email VARCHAR(255),
+  username VARCHAR(255),
+  password_hash VARCHAR(255)
+);
+
+-- Clients table
+DROP TABLE IF EXISTS clients;
+
+CREATE TABLE clients (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email VARCHAR(255),
+  username VARCHAR(255),
+  password_hash VARCHAR(255)
+);
+
 
 INSERT INTO users (first_name, last_name, age, weight, smoker)
 VALUES ('John', 'Doe', 35, 183.7, false);
@@ -20,3 +46,9 @@ VALUES ('Chad', 'Smith', 35, 185.3, true);
 
 INSERT INTO users (first_name, last_name, age, weight, smoker)
 VALUES ('Karen', 'Smith', 33, 159.5, false);
+
+INSERT INTO accountants (first_name, last_name, email, username, password_hash)
+VALUES ('Alex', 'Dawn', 'alex@example.com', 'adawn', 'fakepass1');
+
+INSERT INTO clients (first_name, last_name, email, username, password_hash)
+VALUES ('Kaleb', 'Mallory', 'kaleb@example.com', 'kmall', 'fakepass2');
