6c109a6a356b6ee52d20d1e5f343627ff620dbaa
OblativeShielding <alphanerdling@cox.net>
2026-03-31 21:15:19 -0500

    and fixed it again

--- frontend/src/pages/brochure/Landing.tsx
+++ frontend/src/pages/brochure/Landing.tsx
@@ -1,6 +1,10 @@
+import Container from "@mui/material/Container";
+
 export default function Page() {
   return (
-    <h1>Tax Strategists of America</h1>
-    <p>This landing page will have more on it once we get the styling done.</p>
+    <Container>
+      <h1>Tax Strategists of America</h1>
+      <p>This landing page will have more on it once we get the styling done.</p>
+    </Container>
   );
 };
