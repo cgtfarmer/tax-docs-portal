97449b4a549ebd7df4fb7a06cb7b1f9c1a2040e8
jwsnyder3 <161400606+jwsnyder3@users.noreply.github.com>
2026-04-15 21:30:49 -0500

    Create client message and client task pages, added into main.tsx routes (#36)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -25,6 +25,8 @@ import RegisterPage from "./pages/register/RegisterPage";
 import ClientDashboard from "./pages/clients/ClientDashboard.tsx";
 import ClientAccount from "./pages/clients/ClientAccount.tsx";
 import AccountantHome from './pages/accountants/AccountantHome.tsx';
+import ClientMessages from "./pages/clients/ClientMessages.tsx";
+import ClientTasks from "./pages/clients/ClientTasks.tsx";
 
 const rootElement = document.getElementById("root");
 
@@ -36,9 +38,7 @@ createRoot(rootElement).render(
       <Routes>
         <Route path="/" element={<App />}>
           
-          {/*<Route index element={<Home />} />*/}
-
-          {/* index redirects to landing page (only brochure element as of the time of writing) */}
+          
           <Route index element={<Navigate to="welcome/" replace />} />
 
           <Route path="welcome/" element={<AppBrochure />}>
@@ -47,16 +47,18 @@ createRoot(rootElement).render(
 
           </Route>
 
-          {/* webapp - login page + logged in clients and accountants */}
+        
           <Route path="app" element={<AppLoggedIn />}>
 
             {/* auth routes */}
             <Route path="login" element={<LoginPage />} />
             <Route path="register" element={<RegisterPage />} />
 
-            {/* client dashboard and nested account */}
+            
             <Route path="client" element={<ClientDashboard />}>
               <Route path="account" element={<ClientAccount />} />
+              <Route path="messages" element={<ClientMessages />} />
+              <Route path="tasks" element={<ClientTasks />} />
             </Route>
 
             {/*
