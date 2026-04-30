3e91e54c8e89e1333b9c7b259ee7ec58060b8a47
AnhPhatN <anhphatnguyen9999@gmail.com>
2026-04-29 19:41:22 -0500

    local network testing with multiple devices,
    docker compose up like normal, then connect on other devices with host ip
    
    http://<HOST_IP>:5173/

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,13 @@ export default defineConfig({
 
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['frontend']
+    allowedHosts: ['frontend'],
+    proxy: {
+      '/api': {
+        target: 'http://backend:80',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
